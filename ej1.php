b00d5a378abf591d927d452329ffd4862fa6be60
Katykaze <95656328+Katykaze@users.noreply.github.com>
2022-09-22 18:57:45 +0200

    Update ej1.php

--- ej1.php
+++ ej1.php
@@ -4,6 +4,8 @@
 <?php
 //-------------------- primera forma dividiendo manualmente, solo valdría para este tipo de dirección----------------
 $ip="192.18.16.204";
+//$ip="128.50.39.144";
+//$ip="5.147.220.252";
 $ip1=substr($ip,0,4);
 $ip2=substr($ip,4,3);
 $ip3=substr($ip,7,3);
@@ -17,30 +19,31 @@ $ip4=substr($ip,10,3);
 printf("Numero $ip se representa en binario como %08b.%08b.%08b.%08b <br/>",$ip1 ,$ip2,$ip3,$ip4);
 //-------------------Segunda manera -------------------------
 
-$ipP = strpos($ip,".");
-echo $ipP."<br/><br/><br/>";
-$primero = substr($ip,0,$ipP);
+
+$primero = substr($ip,0,strpos($ip,"."));
 echo $primero."<br/><br/><br/>";
-echo "Numero $primero binario en decimal = ".decbin($primero)."<br/><br/><br/>";
+echo "Numero $primero octal en binario es  = ".decbin($primero)."<br/><br/><br/>";
+//actUalizamos ip quedándonos con el resto de ip
+$ip = substr($ip,strpos($ip,".")+1);
+echo"la ip actualizada  es $ip <br/>";
 
-$nuevaIP=substr($ip,$ipP+1);
-echo $nuevaIP."<br/><br/><br/>";
-$segundo = substr($nuevaIP,0,$ipP-1);
-echo $segundo."<br/><br/><br/>";
-echo "Numero $segundo binario en decimal = ".decbin($segundo)."<br/><br/><br/>";
-
-$nueva2IP = substr($nuevaIP,$ipP);
-echo $nueva2IP."<br/><br/><br/>";
-$tercero = substr($nueva2IP,0,$ipP-1);
+$segundo = substr($ip,0,strpos($ip,"."));
+echo "Numero".$segundo."<br/><br/><br/>";
+echo "Numero $segundo octal en binario es = ".decbin($segundo)."<br/><br/><br/>";
+//actualizamos ip para el siguiente octal
+$ip = substr($ip,strpos($ip,".")+1);
+echo "la nueva $ip <br/>";
+$tercero = substr($ip,0,strpos($ip,"."));
 echo $tercero."<br/><br/><br/>";
 echo "Numero $tercero binario en decimal = ".decbin($tercero)."<br/><br/><br/>";
 
-$nueva3IP = substr($nueva2IP,$ipP);
-echo $nueva3IP."<br/><br/><br/>";
-$cuarta = substr($nueva3IP,0,$ipP);
+$ip = substr($ip,strpos($ip,".")+1);
+echo $ip."<br/><br/><br/>";
+//no es necesario "." porque es el ultimo
+$cuarta = substr($ip,0);
 echo $cuarta."<br/><br/><br/>";
 echo "Numero $cuarta binario en decimal = ".decbin($cuarta)."<br/><br/><br/>";
-printf("Numero $ip se representa en binario como %08b.%08b.%08b.%08b <br/>",$primero ,$segundo,$tercero,$cuarta);
+printf("Numero  se representa en binario como %08b.%08b.%08b.%08b <br/>",$primero ,$segundo,$tercero,$cuarta);
 
 ?>
 </BODY>
