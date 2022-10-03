c7dc563c4047776e6bc14ccdf774b97c1ceb56aa
Katykaze <95656328+Katykaze@users.noreply.github.com>
2022-10-03 10:21:11 +0200

    Update ej9am.php

--- ej9am.php
+++ ej9am.php
@@ -6,39 +6,46 @@
 
 <BODY>
     <?php
-    $matriz = array(array(2, 0, 1,6), array(3, 0, 0,1), array(5, 1, 1,4));
-    for($i=0;$i<count($matriz);$i++){
-        for($j=0;$j<count($matriz[$i]);$j++){
-           echo $matriz[$i][$j];
-           echo " ";
-        }
-        echo "<br>";
-    }
-    printf("<br>");
-    printf("<br>");
+    $matriz = array(array(2, 0, 1, 6), array(3, 0, 0, 1), array(5, 1, 1, 4));
+    echo "Esta es el procedimiento imprimir matriz";
+    echo "</br>";
+    imprimir($matriz);
+
     //---------------matriz traspuesta
-    $matrizTras=array();
-    for($i=0;$i<count($matriz[0]);$i++){
-        $filaTras=array();
-        for($j=0;$j<count($matriz);$j++){
-            //creamos una variable de array para guardar las posiciones ambiadas y las almacenamos en otro array matrizTras
-            $filaTras[]=$matriz[$j][$i];
+    $matrizTras = trasponer($matriz);
+    echo "Imprimiendo matriz traspuesta </br>";
+    imprimir($matrizTras);
+    //--------------------------funciones-------------------------
+  
+    function imprimir($matriz)
+    {
+        for ($i = 0; $i < count($matriz); $i++) {
+            for ($j = 0; $j < count($matriz[$i]); $j++) {
+                echo $matriz[$i][$j];
+                echo " ";
+            }
+            echo "<br>";
         }
-        $matrizTras[]=$filaTras;
+        printf("<br>");
+        printf("<br>");
     }
-    //------------imprimir matriz traspuesta
-    for($i=0;$i<count($matrizTras);$i++){
-        for($j=0;$j<count($matrizTras[$i]);$j++){
-           echo $matrizTras[$i][$j];
-           echo " ";
+
+    function trasponer($matriz){
+        $matrizTras = array();
+        for ($i = 0; $i < count($matriz[0]); $i++) {
+            $filaTras = array();
+            for ($j = 0; $j < count($matriz); $j++) {
+                //creamos una variable de array para guardar las posiciones ambiadas y las almacenamos en otro array matrizTras
+                $filaTras[] = $matriz[$j][$i];
+            }
+            $matrizTras[] = $filaTras;
         }
-        echo "<br>";
+        return $matrizTras;
     }
-    //print_r($matriz);
-    echo "<br>";
-    print_r($matrizTras);
-    echo "<br>";
+
+
     ?>
+
 </BODY>
 
 </HTML>
